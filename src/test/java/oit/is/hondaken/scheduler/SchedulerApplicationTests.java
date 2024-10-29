115498894eb44c637bb61281d928357b6ef0a024
e1b22076 <e1b22076@oit.ac.jp>
2024-10-29 15:00:38 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/hondaken/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.hondaken.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
